e329b4d9b9c508823c149fd8606126a359d1edef
Dương Trần Đức <tranducduong.game@gmail.com>
2024-03-30 20:19:28 +0700

    + Create Enemy

--- Assets/TranDuong/Player/Score.cs
+++ Assets/TranDuong/Player/Score.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("CanhSat"))
         {
-         
+            if (collision.gameObject.transform.position.x < transform.position.x)
+            {
+                print("Cong diem");
+
+            }
+            else if (collision.gameObject.transform.position.x > transform.position.x )
+            {
+                print("tru diem");
+            }
         }
 
+
+        if (collision.gameObject.CompareTag("PlusPoints"))
+        {        
+                print("Cong diem");   
+        }
+
+
+        if (collision.gameObject.CompareTag("MinusPoints"))
+        {
+                print("Tru diem");
+        }
     }
 
 }
